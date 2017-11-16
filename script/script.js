7a94ab4164e3800d3b38a3cb4021d10f5b9ee551
starlord <starlord@github>
2017-11-16 17:06:46 +0530

    A Website about Restaurant

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let text=document.querySelector('.floating-text');
+let body=document.querySelector(body);
+document.addEventListener('onload',type);
+function type(){
+  let length=text.length;
+  let a=0;
+  
+}
