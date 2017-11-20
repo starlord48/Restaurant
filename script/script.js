aa565c240cc0ead635c0ffbe5da1691fcd4b23d4
starlord <starlord@github>
2017-11-20 11:03:49 +0530

    changes in js
    added scroll reveal

--- script/script.js
+++ script/script.js
@@ -1,8 +1,33 @@
-let text=document.querySelector('.floating-text');
-let body=document.querySelector(body);
-document.addEventListener('onload',type);
-function type(){
-  let length=text.length;
-  let a=0;
-  
-}
+window.sr=ScrollReveal();
+sr.reveal('.floating-text',{duration:3000,
+                        origin:'top',
+                        distance:'100px',
+                        scale : 2,
+                        mobile:true});
+                        sr.reveal('.menu',{duration:3000,
+                                                origin:'top',
+                                                distance:'-100px',
+                                                mobile:true
+                                                });
+var  menu=document.querySelector(".menu-list");
+var  menu=document.querySelector(".menu-one");
+var  menu=document.querySelector(".menu-two");
+var  heading=document.querySelector(".floating-text");
+var show=document.querySelector(".show-content");
+show.addEventListener('click',function(){
+  menu.style.display='block';
+heading.style.display='none';
+  sr.reveal('.menu-two',{duration:3000,
+                          origin:'bottom',
+                          distance:'700px',
+
+                          mobile:true});
+                          sr.reveal('.menu-one',{duration:5000,
+                                                  origin:'top',
+                                                  distance:'700px',
+
+                                                  mobile:true});
+
+
+
+});
